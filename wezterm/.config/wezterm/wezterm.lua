e01d28dca1915e9de6fc2f27ac72d08a1d057dab
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2023-05-20 11:57:10 +0200

    update wezterm and neofetch

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,48 +10,34 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Colorscheme
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
-end
--- color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+config.color_scheme = 'Catppuccin Macchiato'
 
--- set custom color overrides
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = 'NeverPrompt'
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
 
--- local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
--- custom.background = "#000000"
--- custom.tab_bar.background = "#040404"
--- custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
--- custom.tab_bar.new_tab.bg_color = "#080808"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 3,
+  bottom = 0,
+}
 
--- return {
---   color_schemes = {
---     ["OLEDppuccin"] = custom,
---   },
---   color_scheme = "OLEDppuccin",
--- }
+config.text_background_opacity = 1
 
+-- wezterm.font('OperatorMonoSSmLig Nerd Font', { weight =370, italic = true })
+config.font = wezterm.font_with_fallback ({
+  { family = "OperatorMonoSSmLig Nerd Font", weight =380 },
+})
+config.use_cap_height_to_scale_fallback_fonts = true
+config.line_height = 1.0
 
--- This is where you actually apply your config choices
-config.window_decorations = "RESIZE",
-config.font = wezterm.font_with_fallback (
-  'Operator Mono SSm Lig',
-  { family = 
-  'font-ligature-symbols',  scale = 0.9 }
-),
-config.use_cap_height_to_scale_fallback_fonts = true,
-config.font_size = 16,
-config.scrollbakc_lines = 5000,
-config.hide_tab_bar_if_only_one_tab = true,
+config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_opacity = 1.0,
-config.text_background_opacity = 0.3,
-
+config.scrollback_lines = 3500
 
 -- and finally, return the configuration to wezterm
 return config
