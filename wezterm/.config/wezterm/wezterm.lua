d31c6cb8ccc7e11b34a1c2659822240f90bbf6e7
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2023-06-05 17:29:39 +0200

    update configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,28 +7,28 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
-
-config.color_scheme = 'Catppuccin Macchiato'
+-- colors.metadata = wezterm.color.load_scheme("wezterm/assets/colors/tokyonight_moon.toml")
+config.color_scheme = "Catppuccin Macchiato"
 
 config.window_decorations = "RESIZE"
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 
 config.window_padding = {
-  left = 1,
-  right = 1,
-  top = 3,
-  bottom = 1,
+	left = 1,
+	right = 0,
+	top = 3,
+	bottom = 0,
 }
 
 config.text_background_opacity = 1
 
 -- wezterm.font('OperatorMonoSSmLig Nerd Font', { weight =370, italic = true })
-config.font = wezterm.font_with_fallback ({
-  { family = "OperatorMonoSSmLig Nerd Font", weight =380 },
+config.font = wezterm.font_with_fallback({
+	{ family = "OperatorMonoSSmLig Nerd Font", weight = 380 },
 })
 config.use_cap_height_to_scale_fallback_fonts = true
 config.line_height = 1.0
