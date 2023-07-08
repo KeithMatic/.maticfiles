dd38db17c7074664642358e0353e69549b03709b
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2023-07-08 10:59:29 +0200

    dd NvChad and LazyVim

--- zsh/.config/zsh/nvims.zsh
+++ zsh/.config/zsh/nvims.zsh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
-alias nvim-kickstart="NVIM_APPNAME=kickstart nvim"
+# alias nvim-kickstart="NVIM_APPNAME=kickstart nvim"
 alias nvim-chad="NVIM_APPNAME=NvChad nvim"
 alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
 
@@ -9,8 +9,16 @@ function nvima() {
   env NVIM_APPNAME=AstroNvim nvim
 }
 
+function nviml() {
+  env NVIM_APPNAME=LazyVim nvim
+}
+
+function nvimn() {
+  env NVIM_APPNAME=NvChad nvim
+}
+
 function nvims() {
-  items=("default" "AstroNvim" "kickstart" "LazyVim" "NvChad")
+  items=("default" "AstroNvim" "LazyVim" "NvChad")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
